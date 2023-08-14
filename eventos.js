b1e813143040e2ee3f7e33b469f845f9e46ed290
AndresMTrz <141237103+AndresMTrz@users.noreply.github.com>
2023-08-13 22:52:28 -0300

    solucion al bug de propagación de eventos

--- eventos.js
+++ eventos.js
@@ -1,2 +1,4 @@
-   const elDiv = document.getElementById('elDiv')
-   elDiv.addEventListener('click', () => alert('Hola! Soy el Div.'))
+   const elDiv = document.getElementById('elDiv');
+   elDiv.addEventListener('click', () => {
+    alert('Hola! Soy el Div.')
+})
